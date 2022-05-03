731a3d84a3ebe7476e7412315cc2a099083a629f
Fablicio Soares <fablicio.soares@gmail.com>
2022-05-03 11:15:35 -0300

    commit

---
+++ variaveis.tf
@@ -0,0 +1,14 @@
+variable "location" {
+    type = string
+    default = "eastus2"
+    description = "localização dos recursos"
+  
+}
+
+variable "name" {
+    type = string
+    default = "fms"
+    description = "nome padrão a ser inserido"
+  
+}
+
